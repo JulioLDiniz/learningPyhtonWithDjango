c1b023a60c2ecbf38103339086d9f7c4c66a83e0
Júlio Liarte Diniz <academicojulio@gmail.com>
2021-06-04 15:20:03 -0300

    Adicionado paginação, busca e alterado banco de dados de SqLite para MySQL

--- project/settings.py
+++ project/settings.py
@@ -75,9 +75,19 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sistema',
+        'PORT': '3306',
+        'HOST': 'localhost',
+        'PASSWORD': 'root',
+        'USER': 'root'
+
     }
 }
 
